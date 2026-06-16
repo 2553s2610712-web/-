e73d779b105cc3bd503dff86733a03c7688c9fb2
kwonmj5475-web <kwonmj5475@gmail.com>
2026-06-16 09:52:21 +0900

    Update 4_권민준.py

--- pages/4_권민준.py
+++ pages/4_권민준.py
@@ -1,1 +1,12 @@
+import streamlit as st
+import pandas as pd
 
+st.set_page_config(page_title="급식 만족도 조사")
+st.title("🍱 오늘 급식 어땠나요? (익명)")
+
+score = st.select_slider("만족도를 선택하세요", options=["😋", "🙂", "😐", "☹️"])
+comment = st.text_area("건의사항 (완전 익명)")
+
+if st.button("투표 제출"):
+    st.success("익명 투표가 완료되었습니다!")
+    st.balloons()
